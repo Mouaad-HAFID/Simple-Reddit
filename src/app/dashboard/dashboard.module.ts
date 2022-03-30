ba727874dfc230a020ab4c1336a32e23681c6f58
Mouaad Hafid <mouaad.mh.hafid@gmail.com>
2022-03-30 22:23:14 +0000

    Updating dahsboard module

--- src/app/dashboard/dashboard.module.ts
+++ src/app/dashboard/dashboard.module.ts
@@ -1,10 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NavbarComponent } from './components/navbar/navbar.component';
+import { HomeComponent } from './components/home/home.component';
+import { HomeContainerComponent } from './containers/home-container/home-container.component';
+import { PostComponent } from './components/post/post.component';
+import { PostContainerComponent } from './containers/post-container/post-container.component';
+import { DashboardTileComponent } from './components/dashboard-tile/dashboard-tile.component';
 
 @NgModule({
-  declarations: [NavbarComponent],
+  declarations: [NavbarComponent, HomeComponent, HomeContainerComponent, PostComponent, PostContainerComponent, DashboardTileComponent],
   imports: [CommonModule],
-  exports: [NavbarComponent],
+  exports: [HomeComponent, NavbarComponent],
 })
 export class DashboardModule {}
