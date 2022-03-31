2b4ae29065bd13376f0e30ccc36ffb35f61fb958
Mouaad Hafid <mouaad.mh.hafid@gmail.com>
2022-03-31 22:11:27 +0000

    Updating components and adding new ones for creation of posts and subreddits

--- src/app/dashboard/components/home/home.component.ts
+++ src/app/dashboard/components/home/home.component.ts
@@ -1,15 +1,14 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { Post } from 'src/app/Models/post.model';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.css'],
 })
 export class HomeComponent implements OnInit {
+  @Input() posts?: Post[];
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
