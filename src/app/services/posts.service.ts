ac78f0029f52db50a33920e050aa5460c140ef3d
Mouaad Hafid <mouaad.mh.hafid@gmail.com>
2022-03-31 22:09:11 +0000

    Adding Services

---
+++ src/app/services/posts.service.ts
@@ -0,0 +1,17 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Post } from '../Models/post.model';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class PostsService {
+  constructor(private http: HttpClient) {}
+  getAllPosts(): Observable<Array<Post>> {
+    return this.http.get<Array<Post>>('http://localhost:3000/posts');
+  }
+  createPost(post: Post): Observable<Post> {
+    return this.http.post<Post>('http://localhost:3000/posts', post);
+  }
+}
---
+++ src/app/services/subreddits.service.ts
@@ -0,0 +1,20 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Subreddit } from '../Models/subreddit.model';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class SubredditsService {
+  constructor(private http: HttpClient) {}
+  getAllSubreddits(): Observable<Array<Subreddit>> {
+    return this.http.get<Array<Subreddit>>('http://localhost:3000/subreddits');
+  }
+  createSubreddit(subreddit: Subreddit): Observable<Subreddit> {
+    return this.http.post<Subreddit>(
+      'http://localhost:3000/subreddits',
+      subreddit
+    );
+  }
+}
