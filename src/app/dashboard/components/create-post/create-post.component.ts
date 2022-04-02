03569b49d156749ab970b15c69c371c3c6593e96
Mouaad Hafid <mouaad.mh.hafid@gmail.com>
2022-04-02 22:12:39 +0000

    Adding creation components and updated styling

--- src/app/dashboard/components/create-post/create-post.component.ts
+++ src/app/dashboard/components/create-post/create-post.component.ts
@@ -1,15 +1,56 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Subscription } from 'rxjs';
+import { Subreddit } from 'src/app/Models/subreddit.model';
+import { PostsService } from 'src/app/services/posts.service';
+import { SubredditsService } from 'src/app/services/subreddits.service';
 
 @Component({
   selector: 'app-create-post',
   templateUrl: './create-post.component.html',
-  styleUrls: ['./create-post.component.css']
+  styleUrls: ['./create-post.component.css'],
 })
-export class CreatePostComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
+export class CreatePostComponent implements OnInit, OnDestroy {
+  subreddits?: Subreddit[];
+  subscription?: Subscription;
+  createPostForm: FormGroup = new FormGroup({
+    title: new FormControl('', [Validators.required, Validators.minLength(3)]),
+    content: new FormControl('', [
+      Validators.required,
+      Validators.minLength(5),
+    ]),
+    subredditId: new FormControl('', Validators.required),
+  });
+  constructor(
+    private subredditService: SubredditsService,
+    private postsService: PostsService
+  ) {}
+  ngOnDestroy(): void {
+    this.subscription?.unsubscribe();
   }
 
+  ngOnInit(): void {
+    this.subscription = this.subredditService
+      .getAllSubreddits()
+      .subscribe((res) => (this.subreddits = res));
+  }
+  showErrors(control: any) {
+    const { dirty, touched, errors } = control;
+    return dirty && touched && errors;
+  }
+  onSubmit() {
+    if (this.createPostForm.valid) {
+      this.postsService
+        .createPost({
+          title: this.createPostForm.value.title,
+          content: this.createPostForm.value.content,
+          subredditId: this.createPostForm.value.subredditId,
+        })
+        .subscribe({
+          complete: () => {
+            this.createPostForm.reset();
+          },
+        });
+    }
+  }
 }
--- src/app/dashboard/components/create-subreddit/create-subreddit.component.ts
+++ src/app/dashboard/components/create-subreddit/create-subreddit.component.ts
@@ -1,15 +1,38 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Subscription } from 'rxjs';
+import { Subreddit } from 'src/app/Models/subreddit.model';
+import { SubredditsService } from 'src/app/services/subreddits.service';
 
 @Component({
   selector: 'app-create-subreddit',
   templateUrl: './create-subreddit.component.html',
-  styleUrls: ['./create-subreddit.component.css']
+  styleUrls: ['./create-subreddit.component.css'],
 })
-export class CreateSubredditComponent implements OnInit {
+export class CreateSubredditComponent implements OnInit, OnDestroy {
+  subscription?: Subscription;
+  subredditForm: FormGroup = new FormGroup({
+    name: new FormControl('', [Validators.required, Validators.minLength(3)]),
+    description: new FormControl('', [Validators.required]),
+  });
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(private subredditsService: SubredditsService) {}
+  ngOnDestroy(): void {
+    this.subscription?.unsubscribe();
   }
-
+  showErrors(control: any) {
+    const { dirty, touched, errors } = control;
+    return dirty && touched && errors;
+  }
+  ngOnInit(): void {}
+  onSubmit() {
+    let subredditToAdd: Subreddit = {
+      name: this.subredditForm.value.name,
+      description: this.subredditForm.value.description,
+    };
+    console.log(subredditToAdd);
+    this.subscription = this.subredditsService
+      .createSubreddit(subredditToAdd)
+      .subscribe({ complete: () => this.subredditForm.reset() });
+  }
 }
