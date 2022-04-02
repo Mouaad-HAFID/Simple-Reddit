8dc65644c6d04a0048b6fc8fd08cf56c639e413b
Mouaad Hafid <mouaad.mh.hafid@gmail.com>
2022-04-02 22:11:10 +0000

    Adding and Updating Routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CreatePostComponent } from './dashboard/components/create-post/create-post.component';
+import { CreateSubredditComponent } from './dashboard/components/create-subreddit/create-subreddit.component';
+import { HomeContainerComponent } from './dashboard/containers/home-container/home-container.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomeContainerComponent },
+  { path: 'subreddit/add', component: CreateSubredditComponent },
+  { path: 'post/add', component: CreatePostComponent },
+  { path: '**', redirectTo: '' },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
