a099f1b9778970328a346d10e50d69dffdcb6762
Mouaad Hafid <mouaad.mh.hafid@gmail.com>
2022-04-06 14:34:45 +0000

    Updating routing and adding env vars

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,14 +2,16 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { CreatePostComponent } from './dashboard/components/create-post/create-post.component';
 import { CreateSubredditComponent } from './dashboard/components/create-subreddit/create-subreddit.component';
-import { PostComponent } from './dashboard/components/post/post.component';
+import { PostDetailsComponent } from './dashboard/components/post-details/post-details.component';
 import { HomeContainerComponent } from './dashboard/containers/home-container/home-container.component';
+import { SubredditContainerComponent } from './dashboard/containers/subreddit-container/subreddit-container.component';
 
 const routes: Routes = [
   { path: '', component: HomeContainerComponent },
   { path: 'subreddit/add', component: CreateSubredditComponent },
+  { path: 'subreddit/:id', component: SubredditContainerComponent },
   { path: 'post/add', component: CreatePostComponent },
-  { path: 'post/{id}', component: PostComponent },
+  { path: 'post/:id', component: PostDetailsComponent },
   { path: '**', redirectTo: '' },
 ];
 
