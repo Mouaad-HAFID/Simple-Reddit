ba4f7db781baa0c470e68cf1120978cda8362e77
Mouaad Hafid <mouaad.mh.hafid@gmail.com>
2022-04-06 14:34:17 +0000

    Updating services

--- src/app/services/posts.service.ts
+++ src/app/services/posts.service.ts
@@ -1,19 +1,22 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
 import { Post } from '../Models/post.model';
 
 @Injectable({
   providedIn: 'root',
 })
 export class PostsService {
+  host = environment.baseUrl;
   constructor(private http: HttpClient) {}
   getAllPosts(): Observable<Array<Post>> {
-    return this.http.get<Array<Post>>(
-      'http://localhost:3000/posts?embed=subreddits'
-    );
+    return this.http.get<Array<Post>>(`${this.host}/posts?_expand=subreddit`);
+  }
+  getPost(id: number): Observable<Post> {
+    return this.http.get<Post>(`${this.host}/posts/${id}?_expand=subreddit`);
   }
   createPost(post: Post): Observable<Post> {
-    return this.http.post<Post>('http://localhost:3000/posts', post);
+    return this.http.post<Post>(`${this.host}/posts`, post);
   }
 }
--- src/app/services/subreddits.service.ts
+++ src/app/services/subreddits.service.ts
@@ -1,22 +1,26 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
 import { Subreddit } from '../Models/subreddit.model';
 
 @Injectable({
   providedIn: 'root',
 })
 export class SubredditsService {
+  host = environment.baseUrl;
   constructor(private http: HttpClient) {}
   getAllSubreddits(): Observable<Array<Subreddit>> {
     return this.http.get<Array<Subreddit>>(
-      'http://localhost:3000/subreddits?_embed=posts'
+      `${this.host}/subreddits?_embed=posts`
+    );
+  }
+  getSubreddit(id: any): Observable<Subreddit> {
+    return this.http.get<Subreddit>(
+      `${this.host}/Subreddits/${id}?_embed=posts`
     );
   }
   createSubreddit(subreddit: Subreddit): Observable<Subreddit> {
-    return this.http.post<Subreddit>(
-      'http://localhost:3000/subreddits',
-      subreddit
-    );
+    return this.http.post<Subreddit>(`${this.host}/subreddits`, subreddit);
   }
 }
